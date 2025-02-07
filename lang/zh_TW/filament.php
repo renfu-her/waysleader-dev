69891ab13a5cf5fc63e91915d62a997c2e67d4b9
Patrick Her <renfu.her@gmail.com>
2025-02-07 10:16:48 +0800

    first commit

---
+++ lang/zh_TW/filament.php
@@ -0,0 +1,65 @@
+<?php
+
+return [
+    'pages' => [
+        'list' => [
+            'title' => '列表',
+        ],
+        'create' => [
+            'title' => '新增',
+        ],
+        'edit' => [
+            'title' => '編輯',
+        ],
+    ],
+    'common' => [
+        'new' => '新增',
+        'no' => '無',
+    ],
+    'table' => [
+        'filters' => [
+            'indicator' => '已選擇的篩選條件',
+        ],
+        'fields' => [
+            'search' => [
+                'indicator' => '搜尋',
+            ],
+        ],
+    ],
+    'pages' => [
+        'actions' => [
+            'create' => [
+                'label' => '新增',
+            ],
+            'edit' => [
+                'label' => '編輯',
+            ],
+            'delete' => [
+                'label' => '刪除',
+            ],
+        ],
+    ],
+    'resources' => [
+        'forms' => [
+            'actions' => [
+                'save' => [
+                    'label' => '儲存',
+                ],
+                'cancel' => [
+                    'label' => '取消',
+                ],
+            ],
+        ],
+        'tables' => [
+            'actions' => [
+                'edit' => [
+                    'label' => '編輯',
+                ],
+                'delete' => [
+                    'label' => '刪除',
+                ],
+            ],
+        ],
+    ],
+
+];
---
+++ lang/zh_TW/pagination.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Pagination Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the paginator library to build
+    | the simple pagination links. You are free to change them to anything
+    | you want to customize your views to better match your application.
+    |
+    */
+
+    'previous' => '&laquo; 上一頁',
+    'next'     => '下一頁 &raquo;',
+    'record_per_page' => [
+        10 => '10 筆',
+        20 => '20 筆',
+        50 => '50 筆',
+        100 => '100 筆',
+        'all' => '全部',
+    ],
+];
