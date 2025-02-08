1a7a6a8af8b7643bf3e5762a389801aff255e881
Patrick Her <renfu.her@gmail.com>
2025-02-08 14:27:56 +0800

    Update API routes and Thunder Client collection
    - Modify PostController route to use route model binding
    - Remove unused Thunder Client collection
    - Add new "About Us" folder and request to API collection

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,6 @@ Route::prefix('v1')->group(function () {
 
     // 文章分類和文章
     Route::get('categories', [PostController::class, 'categories']);
-    Route::get('categories/{categoryId}/posts', [PostController::class, 'categoryPosts']);
+    Route::get('/categories/{category}/posts', [PostController::class, 'index']);
     Route::get('posts/{id}', [PostController::class, 'show']);
 });
