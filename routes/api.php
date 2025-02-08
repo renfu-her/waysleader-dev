630bac5255f2fe358895fd482e99ca27cce8d7c2
Patrick Her <renfu.her@gmail.com>
2025-02-08 20:54:09 +0800

    Add named routes for API endpoints
    - Assign meaningful names to existing API routes
    - Improve route identification and referencing
    - Enhance route organization for Albums, News, and Posts routes

--- routes/api.php
+++ routes/api.php
@@ -8,15 +8,15 @@ use App\Http\Controllers\Api\PostController;
 
 Route::prefix('v1')->group(function () {
     // 相簿
-    Route::get('albums', [AlbumController::class, 'index']);
-    Route::get('albums/{id}', [AlbumController::class, 'show']);
+    Route::get('albums', [AlbumController::class, 'index'])->name('albums.index');
+    Route::get('albums/{id}', [AlbumController::class, 'show'])->name('albums.show');
 
     // 最新消息
-    Route::get('news', [NewsController::class, 'index']);
-    Route::get('news/{id}', [NewsController::class, 'show']);
+    Route::get('news', [NewsController::class, 'index'])->name('news.index');
+    Route::get('news/{id}', [NewsController::class, 'show'])->name('news.show');
 
     // 文章分類和文章
-    Route::get('categories', [PostController::class, 'categories']);
-    Route::get('/categories/{categoryId}', [PostController::class, 'index']);
-    Route::get('posts/{id}', [PostController::class, 'show']);
+    Route::get('categories', [PostController::class, 'categories'])->name('categories.index');
+    Route::get('/categories/{categoryId}', [PostController::class, 'index'])->name('categories.show');
+    Route::get('posts/{id}', [PostController::class, 'show'])->name('posts.show');
 });
