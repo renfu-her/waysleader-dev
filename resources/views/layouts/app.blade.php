1494b97b9917007bcfa20a2d2a1147d5f84002c2
Patrick Her <renfu.her@gmail.com>
2025-02-11 17:21:26 +0800

    Remove scroll-to-top progress indicator from layout
    - Delete SVG-based progress wrap element from app.blade.php layout
    - Simplify page layout by removing unnecessary scroll progress component

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -87,12 +87,6 @@
         </main>
     </div>
 
-    <div class="progress-wrap">
-        <svg class="progress-circle svg-content" width="100%" height="100%" viewbox="-1 -1 102 102">
-            <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98"></path>
-        </svg>
-    </div>
-
     <script>
         $(function() {
             $('.lazy').Lazy();
