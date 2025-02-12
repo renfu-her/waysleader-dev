448f4dbaf5aa9f80d8f7bc13bad09d9562dd8cec
Patrick Her <renfu.her@gmail.com>
2025-02-12 11:08:17 +0800

    Add redirect to index after creating or editing Achievement and AchievementCategory

--- app/Filament/Resources/AchievementCategoryResource/Pages/CreateAchievementCategory.php
+++ app/Filament/Resources/AchievementCategoryResource/Pages/CreateAchievementCategory.php
@@ -8,4 +8,9 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateAchievementCategory extends CreateRecord
 {
     protected static string $resource = AchievementCategoryResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 } 
--- app/Filament/Resources/AchievementCategoryResource/Pages/EditAchievementCategory.php
+++ app/Filament/Resources/AchievementCategoryResource/Pages/EditAchievementCategory.php
@@ -16,4 +16,9 @@ class EditAchievementCategory extends EditRecord
             Actions\DeleteAction::make(),
         ];
     }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 } 
